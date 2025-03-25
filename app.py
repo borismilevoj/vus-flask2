4934f36d7fcae3ca8bca067e13790c27772b499b
bormil <bormil@gmail.com>
2025-03-25 10:54:30 +0100

    Dodana stran /home in predloga home.html

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')  # ⬅️ TO gre tukaj
 
-
 @app.route('/home')
 def home():
-    return '<h1>Deluje – to je direktni response brez predloge!</h1>'
+    return render_template('home.html')
+
 
 @app.route('/admin')
 def admin():
