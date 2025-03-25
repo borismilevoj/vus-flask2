267c37a9939302e14ddbd7704c81c59eb3eeb987
bormil <bormil@gmail.com>
2025-03-25 10:39:08 +0100

    Dodana index.html predloga v templates

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Testna začetna stran deluje!</h1>'
+    return render_template('index.html')  # ⬅️ TO gre tukaj
+
 
 @app.route('/home')
 def home():
