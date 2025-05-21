1cfb3560c6f0bff5d1a41ac0fa48d25ed871df8f
bormil <bormil@gmail.com>
2025-05-21 11:35:24 +0200

    Popravljena navigacija - odpiranje povezav v novem zavihku (target='_blank')

--- krizanka.py
+++ krizanka.py
@@ -25,79 +25,65 @@ def popravi_sumnike(besedilo):
             .replace('Ă§', 'ç')
             .replace('ĂŹ', 'í'))
 
+import xml.etree.ElementTree as ET
+
 def pridobi_podatke_iz_xml(xml_pot):
     tree = ET.parse(xml_pot)
     root = tree.getroot()
-    ns = {'ns': 'http://crossword.info/xml/rectangular-puzzle'}
 
-    grid = root.find('.//ns:grid', ns)
-    width = int(grid.get('width'))
-    height = int(grid.get('height'))
-
-    grid_matrix = [['' for _ in range(width)] for _ in range(height)]
-    for cell in grid.findall(".//ns:cell", ns):
-        x = int(cell.get('x')) - 1
-        y = int(cell.get('y')) - 1
-        char = cell.get('solution')
-        if char:
-            grid_matrix[y][x] = char.upper()
+    grid = root.find('.//{http://crossword.info/xml/rectangular-puzzle}grid')
+    width = int(grid.attrib['width'])
+    height = int(grid.attrib['height'])
 
     crna_polja = []
-    for cell in grid.findall(".//ns:cell", ns):
-        if cell.get('type') == 'block' or not cell.get('solution'):
-            x = int(cell.get('x')) - 1
-            y = int(cell.get('y')) - 1
-            crna_polja.append((x, y))
-
     gesla_opisi = []
-    words = root.findall('.//ns:word', ns)
-    for word in words:
-        geslo_id = word.get('id')
-        x_range = word.get('x')
-        y_range = word.get('y')
 
-        if '-' in x_range:
-            smer = 'across'
-            x_start = int(x_range.split('-')[0]) - 1
-            y_start = int(y_range) - 1
-            dolzina = int(x_range.split('-')[1]) - int(x_range.split('-')[0]) + 1
-        elif '-' in y_range:
-            smer = 'down'
-            x_start = int(x_range) - 1
-            y_start = int(y_range.split('-')[0]) - 1
-            dolzina = int(y_range.split('-')[1]) - int(y_range.split('-')[0]) + 1
-        else:
-            smer = 'across'
-            x_start = int(x_range) - 1
-            y_start = int(y_range) - 1
-            dolzina = 1
+    cells = grid.findall('{http://crossword.info/xml/rectangular-puzzle}cell')
+    cell_numbers = {}
 
-        solution = ''
-        for i in range(dolzina):
-            if smer == 'across':
-                solution += grid_matrix[y_start][x_start + i]
-            else:
-                solution += grid_matrix[y_start + i][x_start]
+    for cell in cells:
+        x = int(cell.attrib.get('x', 0)) - 1
+        y = int(cell.attrib.get('y', 0)) - 1
+        solution = cell.attrib.get('solution')
+        cell_type = cell.attrib.get('type')
 
-        clue = root.find(f".//ns:clue[@word='{geslo_id}']", ns)
-        opis = popravi_sumnike(clue.text.strip()) if clue is not None else "Ni opisa"
-        stevilka = clue.get('number') if clue is not None else "?"
+        if cell_type == 'block' or not solution:
+            crna_polja.append([x, y])
+
+        if 'number' in cell.attrib:
+            cell_numbers[f"{x},{y}"] = cell.attrib['number']  # ključ je zdaj string
+
+    words = root.findall('.//{http://crossword.info/xml/rectangular-puzzle}word')
+    clues = root.findall('.//{http://crossword.info/xml/rectangular-puzzle}clues/{http://crossword.info/xml/rectangular-puzzle}clue')
+
+    for word, clue in zip(words, clues):
+        x_range = word.attrib['x']
+        y_range = word.attrib['y']
+        solution = word.attrib['solution']
+        number = clue.attrib['number']
+        opis = clue.text
+        smer = 'across' if '-' in x_range else 'down'
+
+        x = int(x_range.split('-')[0]) - 1 if '-' in x_range else int(x_range) - 1
+        y = int(y_range.split('-')[0]) - 1 if '-' in y_range else int(y_range) - 1
 
         gesla_opisi.append({
-            'geslo_id': geslo_id,
+            'x': x,
+            'y': y,
+            'stevilka': number,
             'opis': opis,
-            'stevilka': stevilka,
-            'x': x_start,
-            'y': y_start,
-            'smer': smer,
-            'dolzina': dolzina,
             'solution': solution,
-            'je_zacetek': True
+            'smer': smer,
+            'dolzina': len(solution)
         })
 
     return {
         'sirina': width,
         'visina': height,
         'crna_polja': crna_polja,
-        'gesla_opisi': gesla_opisi
+        'gesla_opisi': gesla_opisi,
+        'cell_numbers': cell_numbers
     }
+
+
+
