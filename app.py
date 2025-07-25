b4f01ea2a07a3e051ece9788e8978e017a7d1e1b
bormil <bormil@gmail.com>
2025-07-25 16:46:00 +0200

    Odpravljena absolutna pot do baze za Render

--- app.py
+++ app.py
@@ -67,8 +67,11 @@ def odstrani_cc_vrstico_iz_html(mapa):
                 f.writelines(nove)
             print(f"✂️ Očiščeno: {datoteka}")
 
+import os
 def get_db():
-    conn = sqlite3.connect(r'C:\Users\bormi\Documents\vus-flask2\VUS.db')
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "VUS.db")
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
 
